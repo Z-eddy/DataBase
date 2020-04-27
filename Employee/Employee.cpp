59b1a42103fe6e6d4fc570f43eba8cbefde0d676
zr <576172400@qq.com>
2020-04-27 21:04:13 +0800

    init DB connect

--- Employee/Employee.cpp
+++ Employee/Employee.cpp
@@ -1,7 +1,63 @@
-#include "Employee.h"
+﻿#include "Employee.h"
+#include<iostream>
+#include<QFileDialog>
+#include<QSqlError>
+using std::cout;
+using std::endl;
+using std::ends;
 
 Employee::Employee(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+}
+
+static const QString TABLENAME{ "employee" };//table name
+bool Employee::initConnect()
+{
+	DB_ = QSqlDatabase::addDatabase("QSQLITE", "theSqliteConnectName");
+	if (!DB_.isValid()) {
+		cout <<"invalid:"<< DB_.lastError().text().toStdString() << endl;
+		return false;
+	}
+	DB_.setDatabaseName(TABLENAME);
+	if (!DB_.open()) {//成功打开
+		cout <<"DB open error:"<< DB_.lastError().text().toStdString() << endl;
+		return false;
+	}
+	return true;
+}
+
+void Employee::initModel()
+{
+	theModel = new QSqlTableModel(this, DB_);
+	theSelectionModel = new QItemSelectionModel(theModel);
+}
+
+void Employee::setActState(bool ok)
+{
+	ui.actOpen->setEnabled(!ok);
+	ui.actAppend->setEnabled(ok);
+	ui.actInsert->setEnabled(ok);
+	ui.actDelete->setEnabled(ok);
+	ui.actPhoto->setEnabled(ok);
+	ui.actClear->setEnabled(ok);
+	ui.actScan->setEnabled(ok);
+}
+
+void Employee::on_actOpen_triggered()
+{
+	const QString tempPath{ QFileDialog::getOpenFileName(this,tr("选择数据库"),DBPath_,"DB(*.db3 *.db);;Other(*.*)") };
+	if (tempPath.isEmpty())return;
+	if (DBPath_ != tempPath) {//记忆路径
+		DBPath_ = tempPath;
+	}
+	if (!initConnect())return;
+	initModel();
+	setActState(true);//enable action
+}
+
+void Employee::on_actExit_triggered()
+{
+	this->close();
 }
--- Employee/Employee.h
+++ Employee/Employee.h
@@ -1,6 +1,9 @@
-#pragma once
+﻿#pragma once
 
 #include <QtWidgets/QMainWindow>
+#include<QSqlDatabase>
+#include<QSqlTableModel>
+#include<QItemSelectionModel>
 #include "ui_Employee.h"
 
 class Employee : public QMainWindow
@@ -11,5 +14,21 @@ public:
 	Employee(QWidget *parent = Q_NULLPTR);
 
 private:
+	void init();
+	bool initConnect();//初始化数据库链接
+	void initModel();//初始化view的模型
+	void setActState(bool ok);//设置使用的action状态
+
 	Ui::EmployeeClass ui;
+	QSqlTableModel *theModel;
+	QItemSelectionModel *theSelectionModel;
+	QString DBPath_;
+	QSqlDatabase DB_;
+
+private slots:
+	void on_currentChanged();
+	void on_currentRowChanged();
+
+	void on_actOpen_triggered();//打开数据库
+	void on_actExit_triggered();//退出程序
 };
