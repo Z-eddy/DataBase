ccc70b891019d65ecb7e7c4c59d692f5fb6fde04
zr <576172400@qq.com>
2020-04-28 12:54:19 +0800

    prepare edit initMapper

--- Employee/Employee.cpp
+++ Employee/Employee.cpp
@@ -12,6 +12,20 @@ Employee::Employee(QWidget *parent)
 	ui.setupUi(this);
 }
 
+void Employee::init()
+{
+	if (!initConnect())return;//无法链接则退出
+	initSqlModel();
+	initSelectModel();
+	initMapper();
+	if (theModel_->select()) {
+		setActState(true);//enable action
+	}
+	else {
+		cout << "select error:" << theModel_->lastError().text().toStdString() << endl;
+	}
+}
+
 bool Employee::initConnect()
 {
 	DB_ = QSqlDatabase::addDatabase("QSQLITE", "theSqliteConnectName");
@@ -28,19 +42,47 @@ bool Employee::initConnect()
 }
 
 static const QString TABLENAME{ "employee" };//table name
-void Employee::initModel()
+void Employee::initSqlModel()
 {
-	theModel = new QSqlTableModel(this, DB_);
-	theModel->setEditStrategy(QSqlTableModel::OnManualSubmit);//更改view数据时不自动提交
-	theModel->setTable(TABLENAME);
-	theModel->setSort(0, Qt::AscendingOrder);
-	ui.tabView->setModel(theModel);
+	theModel_ = new QSqlTableModel(this, DB_);
+	theModel_->setEditStrategy(QSqlTableModel::OnManualSubmit);//更改view数据时不自动提交
+	theModel_->setTable(TABLENAME);
+	theModel_->setSort(0, Qt::AscendingOrder);    
 
-	theSelectionModel = new QItemSelectionModel(theModel);
-	ui.tabView->setSelectionModel(theSelectionModel);
-	connect(theSelectionModel, &QItemSelectionModel::currentChanged, this, &Employee::on_currentChanged);
-	connect(theSelectionModel, &QItemSelectionModel::currentRowChanged, this, &Employee::on_currentRowChanged);
-	theModel->select();
+	/*
+	theModel_->setHeaderData(theModel_->fieldIndex("empNo"), Qt::Horizontal, "工号");
+	theModel_->setHeaderData(theModel_->fieldIndex("Name"), Qt::Horizontal, "姓名");
+	theModel_->setHeaderData(theModel_->fieldIndex("Gender"), Qt::Horizontal, "性别");
+	theModel_->setHeaderData(theModel_->fieldIndex("Height"), Qt::Horizontal, "身高");
+	theModel_->setHeaderData(theModel_->fieldIndex("Birthday"), Qt::Horizontal, "出生日期");
+	theModel_->setHeaderData(theModel_->fieldIndex("Mobile"), Qt::Horizontal, "手机");
+	theModel_->setHeaderData(theModel_->fieldIndex("Province"), Qt::Horizontal, "省份");
+	theModel_->setHeaderData(theModel_->fieldIndex("City"), Qt::Horizontal, "城市");
+	theModel_->setHeaderData(theModel_->fieldIndex("Department"), Qt::Horizontal, "部门");
+	theModel_->setHeaderData(theModel_->fieldIndex("Education"), Qt::Horizontal, "学历");
+	theModel_->setHeaderData(theModel_->fieldIndex("Salary"), Qt::Horizontal, "工资");
+
+	theModel_->setHeaderData(theModel_->fieldIndex("Memo"), Qt::Horizontal, "备注"); //这两个字段不再tableView中显示
+	theModel_->setHeaderData(theModel_->fieldIndex("Photo"), Qt::Horizontal, "照片");
+	*/
+
+	ui.tabView->setModel(theModel_);//早设定,否则后续设定可能无效
+	ui.tabView->setColumnHidden(theModel_->fieldIndex("Memo"), true);
+	ui.tabView->setColumnHidden(theModel_->fieldIndex("Photo"), true);
+}
+
+void Employee::initSelectModel()
+{
+	theSelectionModel_ = new QItemSelectionModel(theModel_);
+	ui.tabView->setSelectionModel(theSelectionModel_);
+
+	connect(theSelectionModel_, &QItemSelectionModel::currentChanged, this, &Employee::on_currentChanged);
+	connect(theSelectionModel_, &QItemSelectionModel::currentRowChanged, this, &Employee::on_currentRowChanged);
+}
+
+void Employee::initMapper()
+{
+
 }
 
 void Employee::setActState(bool ok)
@@ -71,9 +113,7 @@ void Employee::on_actOpen_triggered()
 	if (DBPath_ != tempPath) {//记忆路径
 		DBPath_ = tempPath;
 	}
-	if (!initConnect())return;
-	initModel();
-	setActState(true);//enable action
+	init();
 }
 
 void Employee::on_actExit_triggered()
--- Employee/Employee.h
+++ Employee/Employee.h
@@ -1,9 +1,11 @@
 ﻿#pragma once
+#pragma execution_character_set("utf-8")
 
 #include <QtWidgets/QMainWindow>
 #include<QSqlDatabase>
 #include<QSqlTableModel>
 #include<QItemSelectionModel>
+#include<QDataWidgetMapper>
 #include "ui_Employee.h"
 
 class Employee : public QMainWindow
@@ -14,13 +16,17 @@ public:
 	Employee(QWidget *parent = Q_NULLPTR);
 
 private:
-	bool initConnect();//初始化数据库链接
-	void initModel();//初始化view的模型
+	void init();
+		bool initConnect();//初始化数据库链接
+		void initSqlModel();
+		void initSelectModel();//必须先初始化SqlModel,selectModel会使用
+		void initMapper();//关联界面组件与数据模型的字段映射,必须在sqlModel初始化后
 	void setActState(bool ok);//设置使用的action状态
 
 	Ui::EmployeeClass ui;
-	QSqlTableModel *theModel;
-	QItemSelectionModel *theSelectionModel;
+	QSqlTableModel *theModel_;
+	QItemSelectionModel *theSelectionModel_;
+	QDataWidgetMapper *theMapper_;
 	QString DBPath_;
 	QSqlDatabase DB_;
 
